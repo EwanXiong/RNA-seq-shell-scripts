8d7226d8d3078eb5a9b3c5ffff729c4bf1e65cf5
Ivan Xiong <53613810+xyifan97@users.noreply.github.com>
2021-06-10 22:44:05 +0800

    fix bug

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -6,9 +6,9 @@ do
 	java -jar /Date/biosoft/Trimmomatic-0.33/trimmomatic-0.33.jar \
 		PE \
 		-threads 32 \
-		$id.R1.fastq $id.R2.fastq \
-		$id.R1.clean.fastq $id.R1.unpaired.fastq \
-		$id.R2.clean.fastq $id.R2.unpaired.fastq \
+		${id}.R1.fastq ${id}.R2.fastq \
+		${id}.R1.clean.fastq ${id}.R1.unpaired.fastq \
+		${id}.R2.clean.fastq ${id}.R2.unpaired.fastq \
 		ILLUMINACLIP:/Date/biosoft/Trimmomatic-0.33/adapters/adapters.fa:2:30:10 \
 		MINLEN:100 SLIDINGWINDOW:4:15 LEADING:20 AVGQUAL:20 
 done
